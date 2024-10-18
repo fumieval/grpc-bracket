468e831bccb7074e51c8d0962334e7a5ca062b8d
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2024-10-18 13:01:53 +0900

    Remove a duplicated orphan instance
    grpc-haskell-core gives Generic StatusCode now
    https://github.com/awakesecurity/gRPC-haskell/pull/148

--- src/GRPC/Bracket/Orphans.hs
+++ src/GRPC/Bracket/Orphans.hs
@@ -6,6 +6,5 @@ import Data.Aeson
 import GHC.Generics
 import Network.GRPC.HighLevel.Generated as GRPC
 
-deriving instance Generic StatusCode
 instance FromJSON StatusCode
 instance ToJSON StatusCode
